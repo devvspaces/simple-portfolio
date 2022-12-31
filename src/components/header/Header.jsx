a89cd7fc4e8cff5849fcf1f25030a8d152723a29
Afolabi Olajide Samuel <samolajide20@gmail.com>
2022-12-31 12:56:42 +0100

    Updated Role in header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
 			<div className="container header__container">
 				<h5>Hello I'm</h5>
 				<h1>Afolabi Olajide Samuel</h1>
-				<h5 className="text-light">Front-End Developer</h5>
+				<h5 className="text-light">Front-End Engineer</h5>
 				<CTA />
 				<div className="header__lower_section">
 					<HeaderSocials />
